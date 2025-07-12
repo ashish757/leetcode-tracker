164e7218201327a42701241d43efc91822491dd0
Ashish Raj Singh <75162873+ashish757@users.noreply.github.com>
2025-07-12 10:03:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ fetch('/data.json')
   .then(data => {
     console.log("Data from API:", data);
     // Display or use the data as needed
+//     {
+//   "status": "success",
+//   "message": "retrieved",
+//   "totalSolved": 360,
+//   "totalQuestions": 1735,
+//   "easySolved": 146,
+//   "totalEasy": 458,
+//   "mediumSolved": 196,
+//   "totalMedium": 904,
+//   "hardSolved": 21,
+//   "totalHard": 368,
+//   "acceptanceRate": 50.92,
+//   "ranking": 47657,
+//   "contributionPoints": 2534,
+//   "reputation": 1,
+//   "submissionCalendar": {}
+//   }
   });
